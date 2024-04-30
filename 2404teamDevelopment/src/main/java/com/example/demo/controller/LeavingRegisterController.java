558ac43f07e95ed88944ab73e7f78e30dcd0bc59
kazunari-kamichika <155711273+kazunari-kamichika@users.noreply.github.com>
2024-05-01 02:25:53 +0900

    0501

--- 2404teamDevelopment/src/main/java/com/example/demo/controller/LeavingRegisterController.java
+++ 2404teamDevelopment/src/main/java/com/example/demo/controller/LeavingRegisterController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class LeavingRegisterController {
-	
+	  /**
+	   * 退勤登録画面を表示
+	   * @param id 表示する勤怠ID
+	   * @param model Model
+	   * @return 退勤登録画面
+	   */
 	@GetMapping("/featuer/leavingRegister")
 	 public String leavingregister(@PathVariable Integer id,Model model) {
 		
