6f160074c28ff8651972c94eb682749fe23d2c92
kazunari-kamichika <155711273+kazunari-kamichika@users.noreply.github.com>
2024-05-01 02:27:40 +0900

    0501

--- 2404teamDevelopment/src/main/java/com/example/demo/repository/LeavingRegisterRepository.java
+++ 2404teamDevelopment/src/main/java/com/example/demo/repository/LeavingRegisterRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.entity.LeavingRegisterEntity;
 
 /**
- * ユーザー情報 Repository
+ * 勤怠情報 Repository
  */
 @Repository
 public  interface LeavingRegisterRepository extends JpaRepository<LeavingRegisterEntity, Integer> {
