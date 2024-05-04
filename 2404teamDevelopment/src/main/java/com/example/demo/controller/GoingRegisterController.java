e313d1bb3e02e31d614a0a20f12ab0ab15578df4
yutawatanabe0712 <yutaoneok0712@icloud.com>
2024-05-04 23:31:00 +0900

    ユーザーIDをパラメータ受け取り修正（渡邊）

--- 2404teamDevelopment/src/main/java/com/example/demo/controller/GoingRegisterController.java
+++ 2404teamDevelopment/src/main/java/com/example/demo/controller/GoingRegisterController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.dto.GoingRegisterRequest;
@@ -25,33 +26,38 @@ public class GoingRegisterController {
 	/**
 	 * 出勤登録画面を表示する
 	 * @param model モデルオブジェクト
+	 * @param userId 勤怠一覧のレコードと紐づくユーザーIDをパラメータで取得
 	 * @return goingRegister 「goingRegister」という名前のビューへのパス
 	 */
-	@GetMapping("goingRegister/display")
-	public String displayAdd(Model model) {
+	@GetMapping("goingRegister/display/{userId}")
+	public String displayAdd(@PathVariable("userId") Integer userId,Model model) {
 		model.addAttribute("goingRegisterrequest", new GoingRegisterRequest());
+		model.addAttribute("userId", userId);
 		return "goingRegister";
 	}
 
 	/**
 	 * フォームから受け取った出勤登録情報を処理する。エラーがある場合はエラーメッセージと共に出勤登録画面を再表示する。
 	 * @param mode モデルオブジェクト。バリデーションエラーがある場合はエラーメッセージを含む。
+	 * @param userId 勤怠一覧のレコードと紐づくユーザーIDをパラメータで取得
 	 * @param goingRegisterRequest フォームから送信された出勤登録データ
 	 * @param result バリデーション結果を保持する {@link BindingResult} オブジェクト。
 	 * @return 成功時には勤怠一覧確認画面へリダイレクト、エラーがある場合は出勤登録画面へのパス。
 	 */
-	@PostMapping("/goingRegister/create")
-	public String create(@Validated @ModelAttribute GoingRegisterRequest goingRegisterRequest, BindingResult result,
+	@PostMapping("/goingRegister/create/{userId}")
+	public String create(@PathVariable("userId") Integer userId, @Validated @ModelAttribute GoingRegisterRequest goingRegisterRequest, BindingResult result,
 			Model model) {
 
 		if (result.hasErrors()) {
 			addErrorMessagesToModel(result, model);
 			model.addAttribute("goingRegisterrequest", goingRegisterRequest);
+			model.addAttribute("userId", userId);
 			return "goingRegister";
 		}
 
 		goingRegisterService.create(goingRegisterRequest);
 		model.addAttribute("goingRegisterrequest", goingRegisterRequest);
+		model.addAttribute("userId", userId);
 		return "goingRegister";
 	}
 
