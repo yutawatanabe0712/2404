027ee590fe96c33bd57ac1921265181850ef04f1
kazunari-kamichika <155711273+kazunari-kamichika@users.noreply.github.com>
2024-05-14 22:14:17 +0900

    0514

---
+++ 2404teamDevelopment/src/test/java/com/example/demo/controller/UserListControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.example.demo.entity.UserListEntity;
+import com.example.demo.repository.UserListRepository;
+import com.example.demo.service.SampleService;
+
+
+@WebMvcTest(UserListController.class)
+public class UserListControllerTest {
+
+	
+}
