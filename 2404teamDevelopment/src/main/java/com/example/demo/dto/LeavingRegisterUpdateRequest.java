a87e2d2db8ba7de313deb2af13440580fab3bd40
yutawatanabe0712 <yutaoneok0712@icloud.com>
2024-05-15 00:06:57 +0900

    退勤登録マージ

---
+++ 2404teamDevelopment/src/main/java/com/example/demo/dto/LeavingRegisterUpdateRequest.java
@@ -0,0 +1,25 @@
+package com.example.demo.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 退勤情報更新リクエストデータ
+ *
+ */
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class LeavingRegisterUpdateRequest extends LeavingRegisterRequest implements Serializable{
+	  /**
+	   * 勤怠ID
+	   */
+	 @NotNull
+	 private Integer attendance_id;
+	 
+	 
+
+}
